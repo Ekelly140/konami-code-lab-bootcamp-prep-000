c40ba0a89a3f6e40a3e7afb143ab4d34b74571e2
ERIC KELLY <eakelly220@gmail.com>
2018-03-26 21:14:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,14 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const body = document.querySelector('body')
-var check = [] 
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const body = document.querySelector('body');
+var check = [] ;
 
 
 function init() {
   
     body.addEventListener('keydown', function(e) {
-    console.log(e.which)
-  })
+    check.push(e.which)
+    console.log(e.which);
+  });
 
   
 }
