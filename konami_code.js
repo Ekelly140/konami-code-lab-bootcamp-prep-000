2e45a9743629a6a6e5991d5fd626dffdd861e282
ERIC KELLY <eakelly220@gmail.com>
2018-03-26 21:24:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 const body = document.querySelector('body');
 var check = [] ;
-
+var i = 0
 
 function init() {
   
     body.addEventListener('keydown', function(e) {
     check.push(e.which)
     console.log(check);
-    if (check === code){
+    if (check[i] === code[i]){
       console.log("Hurray!")
+      i++
     }
     else if(check.length === code.length){
       check =[]
