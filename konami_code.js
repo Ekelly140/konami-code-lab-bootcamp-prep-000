973ea9712c9ea71ecdde1c2df0d97aea5e4d75ee
ERIC KELLY <eakelly220@gmail.com>
2018-03-26 21:09:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const input = document.querySelector('div')
+const div = document.querySelector('div')
 var check = [] 
 
 
 function init() {
   
-    input.addEventListener('keydown', function(e) {
+    div.addEventListener('keydown', function(e) {
     console.log(e.which)
   })
 
